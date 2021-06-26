ebca20c9a8e7a8d175849948549a01da5f0b6aad
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-06-26 20:24:51 +0530

    Create index.js

---
+++ JavaScript Practice/4. Local Storage/index.js
@@ -0,0 +1,10 @@
+// Local Storage
+
+// in console : use 
+localStorage.setItem('name', 'tushar');
+
+// then for print all elements of the local storage : use
+localStorage;
+
+localStorage.getItem('name');
+// it will print the value of the name : which is tushar
