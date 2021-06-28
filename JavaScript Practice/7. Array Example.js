2ac450c47931d87dbefc19b89166b97c1dc74278
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-06-28 14:37:29 +0530

    Create 7. Array Example.js

---
+++ JavaScript Practice/7. Array Example.js
@@ -0,0 +1,8 @@
+// Store data in Arrays
+
+// example : check for the list of people invited in Party
+var people = ["A", "B", "C", "D", "E"];
+
+// to check whether a element is present in the array or not : use include method
+var checkingName = prompt("Whats your name ?");
+people.includes(checkingName); // this will return boolean value
