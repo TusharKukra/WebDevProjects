5847953d2540f56b387f122479a0a41ecf131618
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-06-28 19:56:08 +0530

    Create 10. Fibonacci Generator .js

---
+++ JavaScript Practice/10. Fibonacci Generator .js
@@ -0,0 +1,27 @@
+// Fibonacci Generator
+
+function fibonacciGenerator(n){
+    var output = [];
+
+    if(n === 1){
+        output = [0];
+    }
+
+    else if(n === 2){
+        output = [0,1];
+    }
+
+    else{
+        output = [0,1];
+
+        // then sum last 2 values
+        for(var i = 2 ; i< n ; i++){
+        output.push(output[output.length-2] + output[output.length-1]);
+        
+        }
+    
+    }
+
+    return output
+
+}
